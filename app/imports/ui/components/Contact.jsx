2b272eb0bdaa5fd8705fc1b0efd9b06107434a48
ula <unichols@hawaii.edu>
2023-10-26 13:03:32 -1000

    Round 3, dont know why this isnt working. my inttellij is not showing any eslint error

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Card, Image } from 'react-bootstrap';
+import CardHeader from 'react-bootstrap/CardHeader';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const StuffItem = ({ stuff }) => (
-  <tr>
-    <td>{stuff.name}</td>
-    <td>{stuff.quantity}</td>
-    <td>{stuff.condition}</td>
-    <td>
-      <Link to={`/edit/${stuff._id}`}>Edit</Link>
-    </td>
-  </tr>
+const Contact = ({ contact }) => (
+  <Card className="h-100">
+    <CardHeader>
+      <Image src={contact.image} width={75} />
+      <Card.Title>{contact.firstName} {contact.lastName}</Card.Title>
+      <Card.Subtitle>{contact.address}</Card.Subtitle>
+    </CardHeader>
+    <Card.Body>
+      <Card.Text>{contact.description}</Card.Text>
+    </Card.Body>
+  </Card>
 );
 
 // Require a document to be passed to this component.
-StuffItem.propTypes = {
-  stuff: PropTypes.shape({
-    name: PropTypes.string,
-    quantity: PropTypes.number,
-    condition: PropTypes.string,
+Contact.propTypes = {
+  contact: PropTypes.shape({
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    address: PropTypes.string,
+    image: PropTypes.string,
+    description: PropTypes.string,
     _id: PropTypes.string,
   }).isRequired,
 };
 
-export default StuffItem;
+export default Contact;
