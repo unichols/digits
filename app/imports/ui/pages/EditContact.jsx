61e68b2e43bc19f496a31f560172fcc495edf49f
ula <unichols@hawaii.edu>
2023-11-02 14:49:18 -1000

    Attempt 1

--- app/imports/ui/pages/EditContact.jsx
+++ app/imports/ui/pages/EditContact.jsx
@@ -42,7 +42,7 @@ const EditContact = () => {
     <Container className="py-3">
       <Row className="justify-content-center">
         <Col xs={5}>
-          <Col className="text-center"><h2>Edit Stuff</h2></Col>
+          <Col className="text-center"><h2>Edit Contact</h2></Col>
           <AutoForm schema={bridge} onSubmit={data => submit(data)} model={doc}>
             <Card>
               <Card.Body>
